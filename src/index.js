b20d479b63b31d7d49b1b760ef87424b46915eb6
Arjav Desai <akdesai912@gmail.com>
2021-05-04 13:19:51 +0530

    added login

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 // core components
 import Admin from "layouts/Admin.js";
 import RTL from "layouts/RTL.js";
+import Login from "layouts/Login.js"
 
 import "assets/css/material-dashboard-react.css?v=1.9.0";
 
@@ -31,9 +32,11 @@ const hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
+      <Route exact path="/login" component={Login}/>
       <Route path="/admin" component={Admin} />
       <Route path="/rtl" component={RTL} />
-      <Redirect from="/" to="/admin/dashboard" />
+      {/* <Redirect from="/" to="/admin/dashboard" /> */}
+      <Redirect from="/" to="/login" />
     </Switch>
   </Router>,
   document.getElementById("root")
